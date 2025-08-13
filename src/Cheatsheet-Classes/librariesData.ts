591f0aab1ca951f8385498cb7cf723396cf40d02
Adam-Elmi <Adamcade123@gmail.com>
2025-08-13 21:30:23 +0300

    Enhanced code & Applied classes

--- src/Cheatsheet-Classes/librariesData.ts
+++ src/Cheatsheet-Classes/librariesData.ts
@@ -1,7 +1,4 @@
-import dynamicPath from "../utils/dynamic_path";
-import { getData, getIndex } from "../utils/handle_data";
-import getProgress from "../utils/progress";
-import icon_detector from "../ReactComponent/helpers/icon_detector";
+import { Library } from "./classData";
 
 /* 
   ------------------
@@ -9,180 +6,18 @@ import icon_detector from "../ReactComponent/helpers/icon_detector";
   ------------------
 */
 const librariesData = [
-  {
-    id: "react",
-    name: "React",
-    targets: "web",
-    get path() {
-      return dynamicPath(this.id);
-    },
-    get icon() {
-      return icon_detector(this.id);
-    },
-    lastUpdate: "Feb 27, 2025",
-    get progress() {
-      return getProgress(getIndex, getData, this);
-    },
-  },
-  {
-    id: "jquery",
-    name: "jQuery",
-    targets: "web",
-    get path() {
-      return dynamicPath(this.id);
-    },
-    get icon() {
-      return icon_detector(this.id);
-    },
-    lastUpdate: "Feb 27, 2025",
-    get progress() {
-      return getProgress(getIndex, getData, this);
-    },
-  },
-  {
-    id: "mongoose",
-    name: "Mongoose",
-    get path() {
-      return dynamicPath(this.id);
-    },
-    get icon() {
-      return icon_detector(this.id);
-    },
-    lastUpdate: "Feb 27, 2025",
-    get progress() {
-      return getProgress(getIndex, getData, this);
-    },
-  },
-  {
-    id: "redux",
-    name: "Redux",
-    targets: "web",
-    get path() {
-      return dynamicPath(this.id);
-    },
-    get icon() {
-      return icon_detector(this.id);
-    },
-    lastUpdate: "Feb 27, 2025",
-    get progress() {
-      return getProgress(getIndex, getData, this);
-    },
-  },
-  {
-    id: "axios",
-    name: "Axios",
-    targets: "web",
-    get path() {
-      return dynamicPath(this.id);
-    },
-    get icon() {
-      return icon_detector(this.id);
-    },
-    lastUpdate: "Feb 27, 2025",
-    get progress() {
-      return getProgress(getIndex, getData, this);
-    },
-  },
-  {
-    id: "prisma",
-    name: "Prisma",
-    get path() {
-      return dynamicPath(this.id);
-    },
-    get icon() {
-      return icon_detector(this.id);
-    },
-    lastUpdate: "Feb 27, 2025",
-    get progress() {
-      return getProgress(getIndex, getData, this);
-    },
-  },
-  {
-    id: "pandas",
-    name: "Pandas",
-    get path() {
-      return dynamicPath(this.id);
-    },
-    get icon() {
-      return icon_detector(this.id);
-    },
-    lastUpdate: "Feb 27, 2025",
-    get progress() {
-      return getProgress(getIndex, getData, this);
-    },
-  },
-  {
-    id: "chartjs",
-    name: "Chart.js",
-    targets: "web",
-    get path() {
-      return dynamicPath(this.id);
-    },
-    get icon() {
-      return icon_detector(this.id);
-    },
-    lastUpdate: "Feb 27, 2025",
-    get progress() {
-      return getProgress(getIndex, getData, this);
-    },
-  },
-  {
-    id: "d3",
-    name: "D3.js",
-    targets: "web",
-    get path() {
-      return dynamicPath(this.id);
-    },
-    get icon() {
-      return icon_detector(this.id);
-    },
-    lastUpdate: "Feb 27, 2025",
-    get progress() {
-      return getProgress(getIndex, getData, this);
-    },
-  },
-  {
-    id: "tensorflow",
-    name: "TensorFlow",
-    get path() {
-      return dynamicPath(this.id);
-    },
-    get icon() {
-      return icon_detector(this.id);
-    },
-    lastUpdate: "Feb 27, 2025",
-    get progress() {
-      return getProgress(getIndex, getData, this);
-    },
-  },
-  {
-    id: "numpy",
-    name: "NumPy",
-    get path() {
-      return dynamicPath(this.id);
-    },
-    get icon() {
-      return icon_detector(this.id);
-    },
-    lastUpdate: "Feb 27, 2025",
-    get progress() {
-      return getProgress(getIndex, getData, this);
-    },
-  },
-  {
-    id: "mdx",
-    name: "MDX",
-    get path() {
-      return dynamicPath(this.id);
-    },
-    get icon() {
-      return icon_detector(this.id);
-    },
-    lastUpdate: "Feb 27, 2025",
-    get progress() {
-      return getProgress(getIndex, getData, this);
-    },
-  },
+  new Library("react", "_", "Feb 27, 2025"),
+  new Library("jquery", "_", "Feb 27, 2025"),
+  new Library("mongoose", "_", "Feb 27, 2025"),
+  new Library("redux", "_", "Feb 27, 2025"),
+  new Library("axios", "_", "Feb 27, 2025"),
+  new Library("prisma", "_", "Feb 27, 2025"),
+  new Library("pandas", "_", "Feb 27, 2025"),
+  new Library("chartjs", "Chart.js", "Feb 27, 2025"),
+  new Library("d3", "D3.js", "Feb 27, 2025"),
+  new Library("tensorflow", "_", "Feb 27, 2025"),
+  new Library("numpy", "NumPy", "Feb 27, 2025"),
+  new Library("mdx", "MDX", "Feb 27, 2025"),
 ];
 
 export default librariesData;
