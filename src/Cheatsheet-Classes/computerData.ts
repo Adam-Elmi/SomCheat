78acbaa59dcc293842213a8f5dcf686226f995e8
Adam-Elmi <Adamcade123@gmail.com>
2025-08-13 17:55:00 +0300

    Enhanced code & Applied classes

---
+++ src/Cheatsheet-Classes/computerData.ts
@@ -0,0 +1,20 @@
+import { BasicComputer } from "./classData";
+
+/*
+  ------------------
+  Computer Data
+  ------------------
+*/
+
+const computerData = [
+  new BasicComputer("binary", "_", "Feb 27, 2025"),
+  new BasicComputer("storage_units", "_", "Feb 27, 2025"),
+  new BasicComputer("hex", "Hexidecimal", "Feb 27, 2025"),
+  new BasicComputer("decimal", "_", "Feb 27, 2025"),
+  new BasicComputer("base_conversion", "_", "Feb 27, 2025"),
+  new BasicComputer("_32bit_and_64bit", "_", "Feb 27, 2025"),
+  new BasicComputer("ascii", "_", "Feb 27, 2025"),
+  new BasicComputer("unicode", "_", "Feb 27, 2025")
+];
+
+export default computerData;
