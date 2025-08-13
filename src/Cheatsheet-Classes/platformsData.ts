bc0576a48ddcc14d382a353da94510b6a00310bf
Adam-Elmi <Adamcade123@gmail.com>
2025-08-13 17:54:13 +0300

    Create platformsData.ts

---
+++ src/Cheatsheet-Classes/platformsData.ts
@@ -0,0 +1,42 @@
+import dynamicPath from "../utils/dynamic_path";
+import { getData, getIndex } from "../utils/handle_data";
+import getProgress from "../utils/progress";
+import icon_detector from "../ReactComponent/helpers/icon_detector";
+
+/* 
+  ------------------
+  Platforms
+  ------------------
+*/
+const platformsData = [
+  {
+    id: "linux",
+    name: "Linux",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "ubuntu",
+    name: "Ubuntu",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+];
+
+export default platformsData;
