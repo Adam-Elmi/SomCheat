5df7a90e0e4fd8cdbbaf9877f9e3dde1e4de2064
Adam-Elmi <Adamcade123@gmail.com>
2025-08-13 18:25:22 +0300

    Fixed a minor issue

---
+++ src/utils/displayMessage.ts
@@ -0,0 +1,19 @@
+import pathData from "../../data/path.json";
+
+const repo: string = "https://github.com/Adam-Elmi/SomCheat";
+document.addEventListener("DOMContentLoaded", () => {
+  const messageContainer: HTMLElement | null = document.getElementById("cheatsheet-message");
+  const pathname: string = window.location.pathname;
+
+  if (messageContainer && pathname.includes("/cheatsheets/")) {
+    const key: string = decodeURIComponent(pathname.split("/").pop() || "")
+    const cheatName: string = (pathData as any)[key].name;
+    messageContainer.innerHTML = `
+    <p>Cheatsheet "<strong>${cheatName}</strong>" not found.
+    <a href=${repo} target="_blank" class="text-blue-500 underline">Contribute</a> or
+    <a href=${repo}/issues target="_blank" class="text-blue-500 underline">Request</a>.</p>
+  `;
+  } else {
+    (messageContainer as HTMLElement).style.display = "none";
+  }
+});
