920aeadcc91ddd8947cc892914fddd00f8fdcee3
Adam-Elmi <Adamcade123@gmail.com>
2025-08-13 17:54:04 +0300

    Create adjustText.ts

---
+++ src/utils/adjustText.ts
@@ -0,0 +1,14 @@
+function adjustText(text: string): string {
+  return (
+    text &&
+    text
+      .split("_")
+      .map((value: string) => {
+        value = value.replace("_", "").replace(/and/gi, "&");
+        return typeof value[0] === "string" ? value[0].toUpperCase() + value.slice(1): null;
+      })
+      .join(" ")
+  );
+}
+
+export default adjustText;
