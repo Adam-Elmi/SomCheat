fa2a4fb61c12b0840e19e28a2738242b7373452f
Adam-Elmi <Adamcade123@gmail.com>
2025-08-13 17:54:17 +0300

    Create librariesData.ts

---
+++ src/Cheatsheet-Classes/librariesData.ts
@@ -0,0 +1,188 @@
+import dynamicPath from "../utils/dynamic_path";
+import { getData, getIndex } from "../utils/handle_data";
+import getProgress from "../utils/progress";
+import icon_detector from "../ReactComponent/helpers/icon_detector";
+
+/* 
+  ------------------
+  Libraries
+  ------------------
+*/
+const librariesData = [
+  {
+    id: "react",
+    name: "React",
+    targets: "web",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "jquery",
+    name: "jQuery",
+    targets: "web",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "mongoose",
+    name: "Mongoose",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "redux",
+    name: "Redux",
+    targets: "web",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "axios",
+    name: "Axios",
+    targets: "web",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "prisma",
+    name: "Prisma",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "pandas",
+    name: "Pandas",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "chartjs",
+    name: "Chart.js",
+    targets: "web",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "d3",
+    name: "D3.js",
+    targets: "web",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "tensorflow",
+    name: "TensorFlow",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "numpy",
+    name: "NumPy",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "mdx",
+    name: "MDX",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+];
+
+export default librariesData;
