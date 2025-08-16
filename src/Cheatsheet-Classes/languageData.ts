55462b518929b27bb029d215ef5c470ca36e9bf9
Adam-Elmi <Adamcade123@gmail.com>
2025-08-16 21:15:38 +0300

    Update languageData.ts

--- src/Cheatsheet-Classes/languageData.ts
+++ src/Cheatsheet-Classes/languageData.ts
@@ -6,6 +6,7 @@ Languages
 ------------------
 */
 const languagesData = [
+  // Scripting languages
   new Language("js", "Javascript", "Feb 27, 2025"),
   new Language("ts", "Typescript", "Feb 27, 2025"),
   new Language("c", "_", "Feb 27, 2025"),
@@ -24,15 +25,26 @@ const languagesData = [
   new Language("dart", "_", "Feb 27, 2025"),
   new Language("ocaml", "_", "Feb 27, 2025"),
   new Language("perl", "_", "Feb 27, 2025"),
+  new Language("nim", "_", "Feb 27, 2025"),
+  new Language("v", "_", "Feb 27, 2025"),
+  new Language("gleam", "_", "Feb 27, 2025"),
+  new Language("julia", "_", "Feb 27, 2025"),
+  new Language("elixir", "_", "Feb 27, 2025"),
+  new Language("erlang", "_", "Feb 27, 2025"),
+  new Language("crystal", "_", "Feb 27, 2025"),
+  new Language("reason", "_", "Feb 27, 2025"),
+  // Shell languages
   new Language("bash", "_", "Feb 27, 2025"),
   new Language("powershell", "_", "Feb 27, 2025"),
   new Language("zsh", "_", "Feb 27, 2025"),
   new Language("fish", "Fish Shell", "Feb 27, 2025"),
+  // Markup languages
   new Language("html", "_", "Feb 27, 2025"),
   new Language("xml", "_", "Feb 27, 2025"),
   new Language("yaml", "_", "Feb 27, 2025"),
   new Language("json", "_", "Feb 27, 2025"),
   new Language("markdown", "_", "Feb 27, 2025"),
+  // Style languages
   new Language("css", "_", "Feb 27, 2025"),
   new Language("sass", "_", "Feb 27, 2025"),
 ];
