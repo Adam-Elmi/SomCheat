d6aa3f8cb5ab5922024661e361fee2f1cff165c7
Adam-Elmi <Adamcade123@gmail.com>
2025-08-16 23:33:38 +0300

    Update platformsData.ts

--- src/Cheatsheet-Classes/platformsData.ts
+++ src/Cheatsheet-Classes/platformsData.ts
@@ -8,6 +8,8 @@ import { Platform } from "./classData";
 const platformsData = [
   new Platform("linux", "_", "Feb 27, 2025"),
   new Platform("ubuntu", "_", "Feb 27, 2025"),
+  new Platform("windows", "_", "Feb 27, 2025"),
+  new Platform("macos", "MacOS", "Feb 27, 2025"),
 ];
 
 export default platformsData;
