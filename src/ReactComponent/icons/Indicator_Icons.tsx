c4390af91410adcee607191cc23514fb10c56a10
Adam-Elmi <Adamcade123@gmail.com>
2025-08-17 23:37:06 +0300

    Update Indicator_Icons.tsx

--- src/ReactComponent/icons/Indicator_Icons.tsx
+++ src/ReactComponent/icons/Indicator_Icons.tsx
@@ -1,18 +1,18 @@
-/* 
+/*
   ------------------
   Default values
   ------------------
 */
 const defaultProps = {
-    dimension: 24,
-    color: "currentColor",
+  dimension: 24,
+  color: "currentColor",
 };
-/* 
+/*
 ------------------
-Alert Icon Component
+Note Icon Component
 ------------------
 */
-export function AlertIcon({
+export function NoteIcon({
   dimension = defaultProps.dimension,
   color = defaultProps.color,
 }) {
@@ -21,18 +21,17 @@ export function AlertIcon({
       xmlns="http://www.w3.org/2000/svg"
       width={dimension}
       height={dimension}
-      viewBox="0 0 22 22"
-      className="inline-block"
+      viewBox="0 0 1024 1024"
     >
       <path
         fill={color}
-        d="M18 20H4v-1H3v-1H2V4h1V3h1V2h14v1h1v1h1v14h-1v1h-1Zm-6-8V6h-2v6Zm0 4v-2h-2v2Z"
+        d="M512 64a448 448 0 1 1 0 896.064A448 448 0 0 1 512 64m67.2 275.072c33.28 0 60.288-23.104 60.288-57.344s-27.072-57.344-60.288-57.344c-33.28 0-60.16 23.104-60.16 57.344s26.88 57.344 60.16 57.344M590.912 699.2c0-6.848 2.368-24.64 1.024-34.752l-52.608 60.544c-10.88 11.456-24.512 19.392-30.912 17.28a12.99 12.99 0 0 1-8.256-14.72l87.68-276.992c7.168-35.136-12.544-67.2-54.336-71.296c-44.096 0-108.992 44.736-148.48 101.504c0 6.784-1.28 23.68.064 33.792l52.544-60.608c10.88-11.328 23.552-19.328 29.952-17.152a12.8 12.8 0 0 1 7.808 16.128L388.48 728.576c-10.048 32.256 8.96 63.872 55.04 71.04c67.84 0 107.904-43.648 147.456-100.416z"
       ></path>
     </svg>
   );
 }
 
-/* 
+/*
 ------------------
 Warning Icon Component
 ------------------
@@ -57,7 +56,7 @@ export function WarningIcon({
   );
 }
 
-/* 
+/*
 ------------------
 Error Icon Component
 ------------------
@@ -72,12 +71,34 @@ export function ErrorIcon({
       width={dimension}
       height={dimension}
       viewBox="0 0 512 512"
-      className="inline-block"
     >
       <path
         fill={color}
         fillRule="evenodd"
         d="M256 42.667c117.803 0 213.334 95.53 213.334 213.333S373.803 469.334 256 469.334S42.667 373.803 42.667 256S138.197 42.667 256 42.667m48.918 134.25L256 225.836l-48.917-48.917l-30.165 30.165L225.835 256l-48.917 48.918l30.165 30.165L256 286.166l48.918 48.917l30.165-30.165L286.166 256l48.917-48.917z"
+      ></path>
+    </svg>
+  );
+}
+/*
+------------------
+Tip Icon Component
+------------------
+*/
+export function TipIcon({
+  dimension = defaultProps.dimension,
+  color = defaultProps.color,
+}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={dimension}
+      height={dimension}
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill={color}
+        d="M12 6a6 6 0 0 1 6 6c0 2.22-1.21 4.16-3 5.2V19a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1v-1.8c-1.79-1.04-3-2.98-3-5.2a6 6 0 0 1 6-6m2 15v1a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-1zm6-10h3v2h-3zM1 11h3v2H1zM13 1v3h-2V1zM4.92 3.5l2.13 2.14l-1.42 1.41L3.5 4.93zm12.03 2.13l2.12-2.13l1.43 1.43l-2.13 2.12z"
       ></path>
     </svg>
   );
