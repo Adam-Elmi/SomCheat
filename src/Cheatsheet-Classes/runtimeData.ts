291bcf8f69e4c90da8ef844eadca60b220ad6fd5
Adam-Elmi <Adamcade123@gmail.com>
2025-08-26 21:19:32 +0300

    Update runtimeData.ts

--- src/Cheatsheet-Classes/runtimeData.ts
+++ src/Cheatsheet-Classes/runtimeData.ts
@@ -1,11 +1,11 @@
 import { Runtime } from "./classData";
-
+import type CheatsheetType from "../types/cheatsheet";
 /* 
   ------------------
   Runtime
   ------------------
 */
-const runtimeData = [
+const runtimeData: CheatsheetType[] = [
   new Runtime("nodejs", "_", "Feb 27, 2025"),
   new Runtime("deno", "_", "Feb 27, 2025"),
   new Runtime("bun", "_", "Feb 27, 2025"),
