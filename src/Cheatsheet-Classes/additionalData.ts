a5592415e20a388bdff5a013c085d09f6aa4cb44
Adam-Elmi <Adamcade123@gmail.com>
2025-08-26 21:20:17 +0300

    Create additionalData.ts

---
+++ src/Cheatsheet-Classes/additionalData.ts
@@ -0,0 +1,13 @@
+import { Additional } from "./classData";
+import type CheatsheetType from "../types/cheatsheet";
+/*
+  ------------------
+  Additional Data
+  ------------------
+*/
+
+const additionalData: CheatsheetType[] = [
+  new Additional("web_api", "Web APIs", "Feb 27, 2025"),
+];
+
+export default additionalData;
