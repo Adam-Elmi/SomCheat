566839dc3cdba13e80786710e742998a2b06195d
Adam-Elmi <Adamcade123@gmail.com>
2025-08-26 21:19:29 +0300

    Update platformsData.ts

--- src/Cheatsheet-Classes/platformsData.ts
+++ src/Cheatsheet-Classes/platformsData.ts
@@ -1,11 +1,11 @@
 import { Platform } from "./classData";
-
+import type CheatsheetType from "../types/cheatsheet";
 /* 
   ------------------
   Platforms
   ------------------
 */
-const platformsData = [
+const platformsData: CheatsheetType[] = [
   new Platform("linux", "_", "Feb 27, 2025"),
   new Platform("ubuntu", "_", "Feb 27, 2025"),
   new Platform("windows", "_", "Feb 27, 2025"),
