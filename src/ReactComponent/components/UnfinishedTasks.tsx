f2251f27ded9fd0289e088d05660af0cd6394602
Adam-Elmi <adamcade123@gmail.com>
2025-09-17 08:04:11 +0300

    Update UnfinishedTasks.tsx

--- src/ReactComponent/components/UnfinishedTasks.tsx
+++ src/ReactComponent/components/UnfinishedTasks.tsx
@@ -35,8 +35,8 @@ export default function UnfinishedTasks({
         className="max-h-0 flex flex-col gap-2 overflow-hidden transition-all duration-300 linear ${
                     "
       >
-        {tasks?.unfinished_t.map((_t: any) => (
-          <div key={"Unfinished-tasks" + _t} className="flex gap-2">
+        {tasks?.unfinished_t.map((_t: any, i: number) => (
+          <div key={"Unfinished-tasks" + _t + i} className="flex gap-2">
             <UncheckIcon dimension={28} color="oklch(0.592 0.249 0.584)" />
             <p className="font-medium dark:text-slate-300 text-slate-600">
               {_t.slice(_t.indexOf(":") + 1)}
