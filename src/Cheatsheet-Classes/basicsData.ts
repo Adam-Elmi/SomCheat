9551e8687ffaebd4aa6df573822bb7389f5b5f47
Adam-Elmi <Adamcade123@gmail.com>
2025-09-27 19:47:37 +0300

    Create basicsData.ts

---
+++ src/Cheatsheet-Classes/basicsData.ts
@@ -0,0 +1,20 @@
+import { BasicsData } from "./classData";
+import type CheatsheetType from "../types/cheatsheet";
+/*
+  ------------------
+  Computer Data
+  ------------------
+*/
+
+const computerData: CheatsheetType[] = [
+  new BasicsData("binary", "_", "Feb 27, 2025"),
+  new BasicsData("storage_units", "_", "Feb 27, 2025"),
+  new BasicsData("hex", "Hexidecimal", "Feb 27, 2025"),
+  new BasicsData("decimal", "_", "Feb 27, 2025"),
+  new BasicsData("base_conversion", "_", "Feb 27, 2025"),
+  new BasicsData("_32bit_and_64bit", "_", "Feb 27, 2025"),
+  new BasicsData("ascii", "_", "Feb 27, 2025"),
+  new BasicsData("unicode", "_", "Feb 27, 2025")
+];
+
+export default computerData;
