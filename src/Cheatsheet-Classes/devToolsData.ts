728dd535ba4064c77ba7068bc6de602dceb93de1
Adam-Elmi <Adamcade123@gmail.com>
2025-09-27 19:47:03 +0300

    Update devToolsData.ts

--- src/Cheatsheet-Classes/devToolsData.ts
+++ src/Cheatsheet-Classes/devToolsData.ts
@@ -1,32 +1,32 @@
-import { DevTools } from "./classData";
+import { DevToolsData } from "./classData";
 import type CheatsheetType from "../types/cheatsheet";
 /* 
   ------------------
-  Dev tools
+  Dev tools Data
   ------------------
 */
 const devToolsData: CheatsheetType[] = [
-  new DevTools("git", "_", "Feb 27, 2025"),
-  new DevTools("vscode", "_", "Feb 27, 2025"),
-  new DevTools("vim", "_", "Feb 27, 2025"),
-  new DevTools("neovim", "_", "Feb 27, 2025"),
-  new DevTools("helix", "_", "Feb 27, 2025"),
-  new DevTools("tmux", "_", "Feb 27, 2025"),
-  new DevTools("curl", "_", "Feb 27, 2025"),
-  new DevTools("vite", "_", "Feb 27, 2025"),
-  new DevTools("webpack", "WebPack", "Feb 27, 2025"),
-  new DevTools("parcel", "_", "Feb 27, 2025"),
-  new DevTools("rollup", "_", "Feb 27, 2025"),
-  new DevTools("rolldown", "_", "Feb 27, 2025"),
-  new DevTools("eslint", "ESLint", "Feb 27, 2025"),
-  new DevTools("babel", "_", "Feb 27, 2025"),
-  new DevTools("prettier", "_", "Feb 27, 2025"),
-  new DevTools("docker", "_", "Feb 27, 2025"),
-  new DevTools("pip", "_", "Feb 27, 2025"),
-  new DevTools("npm", "_", "Feb 27, 2025"),
-  new DevTools("cargo", "_", "Feb 27, 2025"),
-  new DevTools("luarocks", "_", "Feb 27, 2025"),
-  new DevTools("nix", "_", "Feb 27, 2025"),
+  new DevToolsData("git", "_", "Feb 27, 2025"),
+  new DevToolsData("vscode", "_", "Feb 27, 2025"),
+  new DevToolsData("vim", "_", "Feb 27, 2025"),
+  new DevToolsData("neovim", "_", "Feb 27, 2025"),
+  new DevToolsData("helix", "_", "Feb 27, 2025"),
+  new DevToolsData("tmux", "_", "Feb 27, 2025"),
+  new DevToolsData("curl", "_", "Feb 27, 2025"),
+  new DevToolsData("vite", "_", "Feb 27, 2025"),
+  new DevToolsData("webpack", "WebPack", "Feb 27, 2025"),
+  new DevToolsData("parcel", "_", "Feb 27, 2025"),
+  new DevToolsData("rollup", "_", "Feb 27, 2025"),
+  new DevToolsData("rolldown", "_", "Feb 27, 2025"),
+  new DevToolsData("eslint", "ESLint", "Feb 27, 2025"),
+  new DevToolsData("babel", "_", "Feb 27, 2025"),
+  new DevToolsData("prettier", "_", "Feb 27, 2025"),
+  new DevToolsData("docker", "_", "Feb 27, 2025"),
+  new DevToolsData("pip", "_", "Feb 27, 2025"),
+  new DevToolsData("npm", "_", "Feb 27, 2025"),
+  new DevToolsData("cargo", "_", "Feb 27, 2025"),
+  new DevToolsData("luarocks", "_", "Feb 27, 2025"),
+  new DevToolsData("nix", "_", "Feb 27, 2025"),
 ];
 
 export default devToolsData;
