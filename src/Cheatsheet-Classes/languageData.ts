5adbe12bfae9d3b4b37215ac72aaa98897e00338
Adam-Elmi <Adamcade123@gmail.com>
2025-09-27 19:47:09 +0300

    Update languageData.ts

--- src/Cheatsheet-Classes/languageData.ts
+++ src/Cheatsheet-Classes/languageData.ts
@@ -1,52 +1,52 @@
-import { Language } from "./classData";
+import { LanguageData } from "./classData";
 import type CheatsheetType from "../types/cheatsheet";
 /* 
 ------------------
-Languages
+Languages Data
 ------------------
 */
 const languagesData: CheatsheetType[] = [
   // Programming languages
-  new Language("js", "Javascript", "Feb 27, 2025"),
-  new Language("ts", "Typescript", "Feb 27, 2025"),
-  new Language("c", "_", "Feb 27, 2025"),
-  new Language("java", "_", "Feb 27, 2025"),
-  new Language("rust", "_", "Feb 27, 2025"),
-  new Language("swift", "_", "Feb 27, 2025"),
-  new Language("r", "_", "Feb 27, 2025"),
-  new Language("go", "_", "Feb 27, 2025"),
-  new Language("lua", "_", "Feb 27, 2025"),
-  new Language("cs", "C#", "Feb 27, 2025"),
-  new Language("cpp", "C++", "Feb 27, 2025"),
-  new Language("py", "Python", "Feb 27, 2025"),
-  new Language("php", "_", "Feb 27, 2025"),
-  new Language("rb", "Ruby", "Feb 27, 2025"),
-  new Language("kotlin", "_", "Feb 27, 2025"),
-  new Language("dart", "_", "Feb 27, 2025"),
-  new Language("ocaml", "_", "Feb 27, 2025"),
-  new Language("perl", "_", "Feb 27, 2025"),
-  new Language("nim", "_", "Feb 27, 2025"),
-  new Language("v", "_", "Feb 27, 2025"),
-  new Language("gleam", "_", "Feb 27, 2025"),
-  new Language("julia", "_", "Feb 27, 2025"),
-  new Language("elixir", "_", "Feb 27, 2025"),
-  new Language("erlang", "_", "Feb 27, 2025"),
-  new Language("crystal", "_", "Feb 27, 2025"),
-  new Language("reason", "_", "Feb 27, 2025"),
+  new LanguageData("js", "Javascript", "Feb 27, 2025"),
+  new LanguageData("ts", "Typescript", "Feb 27, 2025"),
+  new LanguageData("c", "_", "Feb 27, 2025"),
+  new LanguageData("java", "_", "Feb 27, 2025"),
+  new LanguageData("rust", "_", "Feb 27, 2025"),
+  new LanguageData("swift", "_", "Feb 27, 2025"),
+  new LanguageData("r", "_", "Feb 27, 2025"),
+  new LanguageData("go", "_", "Feb 27, 2025"),
+  new LanguageData("lua", "_", "Feb 27, 2025"),
+  new LanguageData("cs", "C#", "Feb 27, 2025"),
+  new LanguageData("cpp", "C++", "Feb 27, 2025"),
+  new LanguageData("py", "Python", "Feb 27, 2025"),
+  new LanguageData("php", "_", "Feb 27, 2025"),
+  new LanguageData("rb", "Ruby", "Feb 27, 2025"),
+  new LanguageData("kotlin", "_", "Feb 27, 2025"),
+  new LanguageData("dart", "_", "Feb 27, 2025"),
+  new LanguageData("ocaml", "_", "Feb 27, 2025"),
+  new LanguageData("perl", "_", "Feb 27, 2025"),
+  new LanguageData("nim", "_", "Feb 27, 2025"),
+  new LanguageData("v", "_", "Feb 27, 2025"),
+  new LanguageData("gleam", "_", "Feb 27, 2025"),
+  new LanguageData("julia", "_", "Feb 27, 2025"),
+  new LanguageData("elixir", "_", "Feb 27, 2025"),
+  new LanguageData("erlang", "_", "Feb 27, 2025"),
+  new LanguageData("crystal", "_", "Feb 27, 2025"),
+  new LanguageData("reason", "_", "Feb 27, 2025"),
   // Shell languages
-  new Language("bash", "_", "Feb 27, 2025"),
-  new Language("powershell", "_", "Feb 27, 2025"),
-  new Language("zsh", "_", "Feb 27, 2025"),
-  new Language("fish", "Fish Shell", "Feb 27, 2025"),
+  new LanguageData("bash", "_", "Feb 27, 2025"),
+  new LanguageData("powershell", "_", "Feb 27, 2025"),
+  new LanguageData("zsh", "_", "Feb 27, 2025"),
+  new LanguageData("fish", "Fish Shell", "Feb 27, 2025"),
   // Markup languages
-  new Language("html", "_", "Feb 27, 2025"),
-  new Language("xml", "_", "Feb 27, 2025"),
-  new Language("yaml", "_", "Feb 27, 2025"),
-  new Language("json", "_", "Feb 27, 2025"),
-  new Language("markdown", "_", "Feb 27, 2025"),
+  new LanguageData("html", "_", "Feb 27, 2025"),
+  new LanguageData("xml", "_", "Feb 27, 2025"),
+  new LanguageData("yaml", "_", "Feb 27, 2025"),
+  new LanguageData("json", "_", "Feb 27, 2025"),
+  new LanguageData("markdown", "_", "Feb 27, 2025"),
   // Style languages
-  new Language("css", "_", "Feb 27, 2025"),
-  new Language("sass", "_", "Feb 27, 2025"),
+  new LanguageData("css", "_", "Feb 27, 2025"),
+  new LanguageData("sass", "_", "Feb 27, 2025"),
 ];
 
 export default languagesData;
