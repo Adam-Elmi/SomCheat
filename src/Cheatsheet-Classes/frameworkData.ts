6f269c0fe14fd283aa4f91f9bd64b73412ec03b9
Adam-Elmi <Adamcade123@gmail.com>
2025-09-27 19:47:06 +0300

    Update frameworkData.ts

--- src/Cheatsheet-Classes/frameworkData.ts
+++ src/Cheatsheet-Classes/frameworkData.ts
@@ -1,4 +1,4 @@
-import { Framework } from "./classData";
+import { FrameworkData } from "./classData";
 import type CheatsheetType from "../types/cheatsheet";
 /* 
   ------------------
@@ -6,31 +6,31 @@ import type CheatsheetType from "../types/cheatsheet";
   ------------------
 */
 const frameworkData: CheatsheetType[] = [
-  new Framework("expressjs", "Express.js", "Feb 27, 2025"),
-  new Framework("electron", "_", "Feb 27, 2025", "desktop"),
-  new Framework("reactnative", "React Native", "Feb 27, 2025", "mobile"),
-  new Framework("flutter", "_", "Feb 27, 2025", "mobile"),
-  new Framework("vue", "Vue.js", "Feb 27, 2025", "web"),
-  new Framework("angular", "_", "Feb 27, 2025", "web"),
-  new Framework("svelte", "_", "Feb 27, 2025", "web"),
-  new Framework("ember", "Ember.js", "Feb 27, 2025", "web"),
-  new Framework("nextjs", "Next.js", "Feb 27, 2025", "web"),
-  new Framework("nuxtjs", "Nuxt.js", "Feb 27, 2025", "web"),
-  new Framework("spring", "_", "Feb 27, 2025"),
-  new Framework("django", "_", "Feb 27, 2025"),
-  new Framework("laravel", "_", "Feb 27, 2025"),
-  new Framework("rails", "Ruby on Rails", "Feb 27, 2025"),
-  new Framework("flask", "_", "Feb 27, 2025"),
-  new Framework("gin", "_", "Feb 27, 2025"),
-  new Framework("phoenix", "_", "Feb 27, 2025"),
-  new Framework("dotnet", ".NET", "Feb 27, 2025"),
-  new Framework("jest", "_", "Feb 27, 2025"),
-  new Framework("mocha", "_", "Feb 27, 2025"),
-  new Framework("pytest", "_", "Feb 27, 2025"),
-  new Framework("pytorch", "_", "Feb 27, 2025"),
-  new Framework("tailwind", "Tailwind CSS", "Feb 27, 2025"),
-  new Framework("bootstrap", "_", "Feb 27, 2025"),
-  new Framework("bulma", "_", "Feb 27, 2025"),
+  new FrameworkData("expressjs", "Express.js", "Feb 27, 2025"),
+  new FrameworkData("electron", "_", "Feb 27, 2025", "desktop"),
+  new FrameworkData("reactnative", "React Native", "Feb 27, 2025", "mobile"),
+  new FrameworkData("flutter", "_", "Feb 27, 2025", "mobile"),
+  new FrameworkData("vue", "Vue.js", "Feb 27, 2025", "web"),
+  new FrameworkData("angular", "_", "Feb 27, 2025", "web"),
+  new FrameworkData("svelte", "_", "Feb 27, 2025", "web"),
+  new FrameworkData("ember", "Ember.js", "Feb 27, 2025", "web"),
+  new FrameworkData("nextjs", "Next.js", "Feb 27, 2025", "web"),
+  new FrameworkData("nuxtjs", "Nuxt.js", "Feb 27, 2025", "web"),
+  new FrameworkData("spring", "_", "Feb 27, 2025"),
+  new FrameworkData("django", "_", "Feb 27, 2025"),
+  new FrameworkData("laravel", "_", "Feb 27, 2025"),
+  new FrameworkData("rails", "Ruby on Rails", "Feb 27, 2025"),
+  new FrameworkData("flask", "_", "Feb 27, 2025"),
+  new FrameworkData("gin", "_", "Feb 27, 2025"),
+  new FrameworkData("phoenix", "_", "Feb 27, 2025"),
+  new FrameworkData("dotnet", ".NET", "Feb 27, 2025"),
+  new FrameworkData("jest", "_", "Feb 27, 2025"),
+  new FrameworkData("mocha", "_", "Feb 27, 2025"),
+  new FrameworkData("pytest", "_", "Feb 27, 2025"),
+  new FrameworkData("pytorch", "_", "Feb 27, 2025"),
+  new FrameworkData("tailwind", "Tailwind CSS", "Feb 27, 2025"),
+  new FrameworkData("bootstrap", "_", "Feb 27, 2025"),
+  new FrameworkData("bulma", "_", "Feb 27, 2025"),
 ];
 
 export default frameworkData;
