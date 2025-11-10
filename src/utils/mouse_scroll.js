a606e28b952e42ce08ed9034aa22d693de81b41b
Adam-Elmi <Adamcade123@gmail.com>
2025-11-10 19:07:07 +0300

    Create mouse_scroll.js

---
+++ src/utils/mouse_scroll.js
@@ -0,0 +1,30 @@
+const slider = document.getElementById("scroll-box");
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("cursor-grabbing");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("cursor-grabbing");
+});
+
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("cursor-grabbing");
+});
+
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX); // pixels moved
+  slider.scrollLeft = scrollLeft - walk;
+});
