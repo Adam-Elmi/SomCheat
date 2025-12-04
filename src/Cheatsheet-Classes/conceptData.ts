3a53379266b649d9540462232e77a70539d1a1d7
Adam-Elmi <Adamcade123@gmail.com>
2025-12-05 02:40:11 +0300

    Create conceptData.ts

---
+++ src/Cheatsheet-Classes/conceptData.ts
@@ -0,0 +1,20 @@
+import { ConceptData } from "./cheatsheetData";
+import type CheatsheetType from "../types/cheatsheet";
+/*
+  ------------------
+  Concepts Data
+  ------------------
+*/
+
+const conceptData: CheatsheetType[] = [
+  new ConceptData("binary", "_", "Feb 27, 2025"),
+  new ConceptData("storage_units", "_", "Feb 27, 2025"),
+  new ConceptData("hex", "Hexidecimal", "Feb 27, 2025"),
+  new ConceptData("decimal", "_", "Feb 27, 2025"),
+  new ConceptData("base_conversion", "_", "Feb 27, 2025"),
+  new ConceptData("_32bit_and_64bit", "_", "Feb 27, 2025"),
+  new ConceptData("ascii", "_", "Feb 27, 2025"),
+  new ConceptData("unicode", "_", "Feb 27, 2025")
+];
+
+export default conceptData;
