8c9d570615283b02abc14e9c4b03991f3d4f395c
Adam-Elmi <Adamcade123@gmail.com>
2026-01-13 02:41:22 +0300

    created commits.sh

---
+++ commits.sh
@@ -0,0 +1,232 @@
+#!/bin/bash
+set -e
+git add commits.sh
+git commit -m "created commits.sh"
+git add generate_commits.py
+git commit -m "created generate_commits.py"
+git add src/pages/cheatsheets/angular.mdx
+git commit -m "created angular.mdx"
+git add src/pages/cheatsheets/ascii.mdx
+git commit -m "created ascii.mdx"
+git add src/pages/cheatsheets/axios.mdx
+git commit -m "created axios.mdx"
+git add src/pages/cheatsheets/babel.mdx
+git commit -m "created babel.mdx"
+git add src/pages/cheatsheets/base_conversion.mdx
+git commit -m "created base_conversion.mdx"
+git add src/pages/cheatsheets/bash.mdx
+git commit -m "created bash.mdx"
+git add src/pages/cheatsheets/binary.mdx
+git commit -m "created binary.mdx"
+git add src/pages/cheatsheets/bootstrap.mdx
+git commit -m "created bootstrap.mdx"
+git add src/pages/cheatsheets/bulma.mdx
+git commit -m "created bulma.mdx"
+git add src/pages/cheatsheets/bun.mdx
+git commit -m "created bun.mdx"
+git add src/pages/cheatsheets/cargo.mdx
+git commit -m "created cargo.mdx"
+git add src/pages/cheatsheets/chartjs.mdx
+git commit -m "created chartjs.mdx"
+git add src/pages/cheatsheets/cpp.mdx
+git commit -m "created cpp.mdx"
+git add src/pages/cheatsheets/crystal.mdx
+git commit -m "created crystal.mdx"
+git add src/pages/cheatsheets/cs.mdx
+git commit -m "created cs.mdx"
+git add src/pages/cheatsheets/css.mdx
+git commit -m "created css.mdx"
+git add src/pages/cheatsheets/curl.mdx
+git commit -m "created curl.mdx"
+git add src/pages/cheatsheets/d3.mdx
+git commit -m "created d3.mdx"
+git add src/pages/cheatsheets/dart.mdx
+git commit -m "created dart.mdx"
+git add src/pages/cheatsheets/decimal.mdx
+git commit -m "created decimal.mdx"
+git add src/pages/cheatsheets/deno.mdx
+git commit -m "created deno.mdx"
+git add src/pages/cheatsheets/django.mdx
+git commit -m "created django.mdx"
+git add src/pages/cheatsheets/docker.mdx
+git commit -m "created docker.mdx"
+git add src/pages/cheatsheets/dotnet.mdx
+git commit -m "created dotnet.mdx"
+git add src/pages/cheatsheets/electron.mdx
+git commit -m "created electron.mdx"
+git add src/pages/cheatsheets/elixir.mdx
+git commit -m "created elixir.mdx"
+git add src/pages/cheatsheets/ember.mdx
+git commit -m "created ember.mdx"
+git add src/pages/cheatsheets/erlang.mdx
+git commit -m "created erlang.mdx"
+git add src/pages/cheatsheets/eslint.mdx
+git commit -m "created eslint.mdx"
+git add src/pages/cheatsheets/expressjs.mdx
+git commit -m "created expressjs.mdx"
+git add src/pages/cheatsheets/firestore.mdx
+git commit -m "created firestore.mdx"
+git add src/pages/cheatsheets/fish.mdx
+git commit -m "created fish.mdx"
+git add src/pages/cheatsheets/flask.mdx
+git commit -m "created flask.mdx"
+git add src/pages/cheatsheets/flutter.mdx
+git commit -m "created flutter.mdx"
+git add src/pages/cheatsheets/gin.mdx
+git commit -m "created gin.mdx"
+git add src/pages/cheatsheets/git.mdx
+git commit -m "created git.mdx"
+git add src/pages/cheatsheets/gleam.mdx
+git commit -m "created gleam.mdx"
+git add src/pages/cheatsheets/helix.mdx
+git commit -m "created helix.mdx"
+git add src/pages/cheatsheets/hex.mdx
+git commit -m "created hex.mdx"
+git add src/pages/cheatsheets/html.mdx
+git commit -m "created html.mdx"
+git add src/pages/cheatsheets/java.mdx
+git commit -m "created java.mdx"
+git add src/pages/cheatsheets/jest.mdx
+git commit -m "created jest.mdx"
+git add src/pages/cheatsheets/jquery.mdx
+git commit -m "created jquery.mdx"
+git add src/pages/cheatsheets/json.mdx
+git commit -m "created json.mdx"
+git add src/pages/cheatsheets/julia.mdx
+git commit -m "created julia.mdx"
+git add src/pages/cheatsheets/kotlin.mdx
+git commit -m "created kotlin.mdx"
+git add src/pages/cheatsheets/laravel.mdx
+git commit -m "created laravel.mdx"
+git add src/pages/cheatsheets/linux.mdx
+git commit -m "created linux.mdx"
+git add src/pages/cheatsheets/luarocks.mdx
+git commit -m "created luarocks.mdx"
+git add src/pages/cheatsheets/macos.mdx
+git commit -m "created macos.mdx"
+git add src/pages/cheatsheets/markdown.mdx
+git commit -m "created markdown.mdx"
+git add src/pages/cheatsheets/mdx.mdx
+git commit -m "created mdx.mdx"
+git add src/pages/cheatsheets/mocha.mdx
+git commit -m "created mocha.mdx"
+git add src/pages/cheatsheets/mongodb.mdx
+git commit -m "created mongodb.mdx"
+git add src/pages/cheatsheets/mongoose.mdx
+git commit -m "created mongoose.mdx"
+git add src/pages/cheatsheets/mysql.mdx
+git commit -m "created mysql.mdx"
+git add src/pages/cheatsheets/neovim.mdx
+git commit -m "created neovim.mdx"
+git add src/pages/cheatsheets/nextjs.mdx
+git commit -m "created nextjs.mdx"
+git add src/pages/cheatsheets/nim.mdx
+git commit -m "created nim.mdx"
+git add src/pages/cheatsheets/nix.mdx
+git commit -m "created nix.mdx"
+git add src/pages/cheatsheets/nixos.mdx
+git commit -m "created nixos.mdx"
+git add src/pages/cheatsheets/nodejs.mdx
+git commit -m "created nodejs.mdx"
+git add src/pages/cheatsheets/npm.mdx
+git commit -m "created npm.mdx"
+git add src/pages/cheatsheets/numpy.mdx
+git commit -m "created numpy.mdx"
+git add src/pages/cheatsheets/nuxtjs.mdx
+git commit -m "created nuxtjs.mdx"
+git add src/pages/cheatsheets/ocaml.mdx
+git commit -m "created ocaml.mdx"
+git add src/pages/cheatsheets/pandas.mdx
+git commit -m "created pandas.mdx"
+git add src/pages/cheatsheets/parcel.mdx
+git commit -m "created parcel.mdx"
+git add src/pages/cheatsheets/phoenix.mdx
+git commit -m "created phoenix.mdx"
+git add src/pages/cheatsheets/php.mdx
+git commit -m "created php.mdx"
+git add src/pages/cheatsheets/pip.mdx
+git commit -m "created pip.mdx"
+git add src/pages/cheatsheets/postgresql.mdx
+git commit -m "created postgresql.mdx"
+git add src/pages/cheatsheets/powershell.mdx
+git commit -m "created powershell.mdx"
+git add src/pages/cheatsheets/prettier.mdx
+git commit -m "created prettier.mdx"
+git add src/pages/cheatsheets/prisma.mdx
+git commit -m "created prisma.mdx"
+git add src/pages/cheatsheets/pytest.mdx
+git commit -m "created pytest.mdx"
+git add src/pages/cheatsheets/pytorch.mdx
+git commit -m "created pytorch.mdx"
+git add src/pages/cheatsheets/r.mdx
+git commit -m "created r.mdx"
+git add src/pages/cheatsheets/rails.mdx
+git commit -m "created rails.mdx"
+git add src/pages/cheatsheets/rb.mdx
+git commit -m "created rb.mdx"
+git add src/pages/cheatsheets/react.mdx
+git commit -m "created react.mdx"
+git add src/pages/cheatsheets/reactnative.mdx
+git commit -m "created reactnative.mdx"
+git add src/pages/cheatsheets/reason.mdx
+git commit -m "created reason.mdx"
+git add src/pages/cheatsheets/redis.mdx
+git commit -m "created redis.mdx"
+git add src/pages/cheatsheets/redux.mdx
+git commit -m "created redux.mdx"
+git add src/pages/cheatsheets/rolldown.mdx
+git commit -m "created rolldown.mdx"
+git add src/pages/cheatsheets/rollup.mdx
+git commit -m "created rollup.mdx"
+git add src/pages/cheatsheets/rust.mdx
+git commit -m "created rust.mdx"
+git add src/pages/cheatsheets/sass.mdx
+git commit -m "created sass.mdx"
+git add src/pages/cheatsheets/spring.mdx
+git commit -m "created spring.mdx"
+git add src/pages/cheatsheets/sqlite.mdx
+git commit -m "created sqlite.mdx"
+git add src/pages/cheatsheets/storage_units.mdx
+git commit -m "created storage_units.mdx"
+git add src/pages/cheatsheets/svelte.mdx
+git commit -m "created svelte.mdx"
+git add src/pages/cheatsheets/swift.mdx
+git commit -m "created swift.mdx"
+git add src/pages/cheatsheets/tailwind.mdx
+git commit -m "created tailwind.mdx"
+git add src/pages/cheatsheets/tensorflow.mdx
+git commit -m "created tensorflow.mdx"
+git add src/pages/cheatsheets/tmux.mdx
+git commit -m "created tmux.mdx"
+git add src/pages/cheatsheets/ts.mdx
+git commit -m "created ts.mdx"
+git add src/pages/cheatsheets/ubuntu.mdx
+git commit -m "created ubuntu.mdx"
+git add src/pages/cheatsheets/unicode.mdx
+git commit -m "created unicode.mdx"
+git add src/pages/cheatsheets/v.mdx
+git commit -m "created v.mdx"
+git add src/pages/cheatsheets/vim.mdx
+git commit -m "created vim.mdx"
+git add src/pages/cheatsheets/vite.mdx
+git commit -m "created vite.mdx"
+git add src/pages/cheatsheets/vscode.mdx
+git commit -m "created vscode.mdx"
+git add src/pages/cheatsheets/vue.mdx
+git commit -m "created vue.mdx"
+git add src/pages/cheatsheets/web_api.mdx
+git commit -m "created web_api.mdx"
+git add src/pages/cheatsheets/webpack.mdx
+git commit -m "created webpack.mdx"
+git add src/pages/cheatsheets/windows.mdx
+git commit -m "created windows.mdx"
+git add src/pages/cheatsheets/xml.mdx
+git commit -m "created xml.mdx"
+git add src/pages/cheatsheets/yaml.mdx
+git commit -m "created yaml.mdx"
+git add src/pages/cheatsheets/zsh.mdx
+git commit -m "created zsh.mdx"
+git add src/scripts/
+git commit -m "created src/scripts directory"
+git add src/utils/formatDate.ts
+git commit -m "created formatDate.ts"
