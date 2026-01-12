f926823906b3abc845b1132b2ce5b3656110828d
Adam-Elmi <Adamcade123@gmail.com>
2026-01-13 02:38:57 +0300

    updated conceptData.ts

--- src/Cheatsheet-Classes/conceptData.ts
+++ src/Cheatsheet-Classes/conceptData.ts
@@ -1,20 +1,16 @@
 import { ConceptData } from "./cheatsheetData";
 import type CheatsheetType from "../types/cheatsheet";
+import { getAllCheatsheets } from "../data/cheatsheets";
+
 /*
   ------------------
   Concepts Data
   ------------------
 */
+const all = getAllCheatsheets();
 
-const conceptData: CheatsheetType[] = [
-  new ConceptData("binary", "_", "Feb 27, 2025"),
-  new ConceptData("storage_units", "_", "Feb 27, 2025"),
-  new ConceptData("hex", "Hexidecimal", "Feb 27, 2025"),
-  new ConceptData("decimal", "_", "Feb 27, 2025"),
-  new ConceptData("base_conversion", "_", "Feb 27, 2025"),
-  new ConceptData("_32bit_and_64bit", "_", "Feb 27, 2025"),
-  new ConceptData("ascii", "_", "Feb 27, 2025"),
-  new ConceptData("unicode", "_", "Feb 27, 2025")
-];
+const conceptData: CheatsheetType[] = all
+  .filter(c => c.category === "Concept")
+  .map(c => new ConceptData(c.id, c.title, "Unknown", undefined, c.lastModified));
 
 export default conceptData;
