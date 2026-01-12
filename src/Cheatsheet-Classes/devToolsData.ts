aadc277ef0305417e054d5a0a58fa3b26ee31bb3
Adam-Elmi <Adamcade123@gmail.com>
2026-01-13 02:38:57 +0300

    updated devToolsData.ts

--- src/Cheatsheet-Classes/devToolsData.ts
+++ src/Cheatsheet-Classes/devToolsData.ts
@@ -1,32 +1,16 @@
 import { DevToolsData } from "./cheatsheetData";
 import type CheatsheetType from "../types/cheatsheet";
+import { getAllCheatsheets } from "../data/cheatsheets";
+
 /* 
   ------------------
   Dev tools Data
   ------------------
 */
-const devToolsData: CheatsheetType[] = [
-  new DevToolsData("git", "_", "Feb 27, 2025"),
-  new DevToolsData("vscode", "_", "Feb 27, 2025"),
-  new DevToolsData("vim", "_", "Feb 27, 2025"),
-  new DevToolsData("neovim", "_", "Feb 27, 2025"),
-  new DevToolsData("helix", "_", "Feb 27, 2025"),
-  new DevToolsData("tmux", "_", "Feb 27, 2025"),
-  new DevToolsData("curl", "_", "Feb 27, 2025"),
-  new DevToolsData("vite", "_", "Feb 27, 2025"),
-  new DevToolsData("webpack", "WebPack", "Feb 27, 2025"),
-  new DevToolsData("parcel", "_", "Feb 27, 2025"),
-  new DevToolsData("rollup", "_", "Feb 27, 2025"),
-  new DevToolsData("rolldown", "_", "Feb 27, 2025"),
-  new DevToolsData("eslint", "ESLint", "Feb 27, 2025"),
-  new DevToolsData("babel", "_", "Feb 27, 2025"),
-  new DevToolsData("prettier", "_", "Feb 27, 2025"),
-  new DevToolsData("docker", "_", "Feb 27, 2025"),
-  new DevToolsData("pip", "_", "Feb 27, 2025"),
-  new DevToolsData("npm", "_", "Feb 27, 2025"),
-  new DevToolsData("cargo", "_", "Feb 27, 2025"),
-  new DevToolsData("luarocks", "_", "Feb 27, 2025"),
-  new DevToolsData("nix", "_", "Feb 27, 2025"),
-];
+const all = getAllCheatsheets();
+
+const devToolsData: CheatsheetType[] = all
+  .filter(c => c.category === "DevTools")
+  .map(c => new DevToolsData(c.id, c.title, "Unknown", undefined, c.lastModified));
 
 export default devToolsData;
