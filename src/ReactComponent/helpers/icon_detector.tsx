1f17c67d8f4b3223062890815858c1bd5c3f57fa
Adam-Elmi <Adamcade123@gmail.com>
2026-01-13 02:38:58 +0300

    updated icon_detector.tsx

--- src/ReactComponent/helpers/icon_detector.tsx
+++ src/ReactComponent/helpers/icon_detector.tsx
@@ -1,17 +1,13 @@
-import pathJson from "../../../data/path.json";
 import all_cheat_icons from "./list_icons";
 
 export default function icon_detector(cheatname: string) {
   try {
-    if (pathJson && all_cheat_icons) {
-      for (const [key, _] of Object.entries(pathJson)) {
-        if (cheatname === key && all_cheat_icons.hasOwnProperty(key)) {
-          return all_cheat_icons[key as keyof typeof all_cheat_icons];
-        }
-      }
+    if (all_cheat_icons && all_cheat_icons.hasOwnProperty(cheatname)) {
+      return all_cheat_icons[cheatname as keyof typeof all_cheat_icons];
     }
     return all_cheat_icons.default;
   } catch (error) {
     console.error(error);
+    return all_cheat_icons.default;
   }
 }
