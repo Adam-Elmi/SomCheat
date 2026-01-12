401a4ffee7550e1760fea20b25eb828e6c6fae10
Adam-Elmi <Adamcade123@gmail.com>
2026-01-13 02:38:57 +0300

    updated languageData.ts

--- src/Cheatsheet-Classes/languageData.ts
+++ src/Cheatsheet-Classes/languageData.ts
@@ -1,52 +1,16 @@
 import { LanguageData } from "./cheatsheetData";
 import type CheatsheetType from "../types/cheatsheet";
+import { getAllCheatsheets } from "../data/cheatsheets";
+
 /* 
 ------------------
 Languages Data
 ------------------
 */
-const languagesData: CheatsheetType[] = [
-  // Programming languages
-  new LanguageData("js", "Javascript", "Feb 27, 2025"),
-  new LanguageData("ts", "Typescript", "Feb 27, 2025"),
-  new LanguageData("c", "_", "Feb 27, 2025"),
-  new LanguageData("java", "_", "Feb 27, 2025"),
-  new LanguageData("rust", "_", "Feb 27, 2025"),
-  new LanguageData("swift", "_", "Feb 27, 2025"),
-  new LanguageData("r", "_", "Feb 27, 2025"),
-  new LanguageData("go", "_", "Feb 27, 2025"),
-  new LanguageData("lua", "_", "Feb 27, 2025"),
-  new LanguageData("cs", "C#", "Feb 27, 2025"),
-  new LanguageData("cpp", "C++", "Feb 27, 2025"),
-  new LanguageData("py", "Python", "Feb 27, 2025"),
-  new LanguageData("php", "_", "Feb 27, 2025"),
-  new LanguageData("rb", "Ruby", "Feb 27, 2025"),
-  new LanguageData("kotlin", "_", "Feb 27, 2025"),
-  new LanguageData("dart", "_", "Feb 27, 2025"),
-  new LanguageData("ocaml", "_", "Feb 27, 2025"),
-  new LanguageData("perl", "_", "Feb 27, 2025"),
-  new LanguageData("nim", "_", "Feb 27, 2025"),
-  new LanguageData("v", "_", "Feb 27, 2025"),
-  new LanguageData("gleam", "_", "Feb 27, 2025"),
-  new LanguageData("julia", "_", "Feb 27, 2025"),
-  new LanguageData("elixir", "_", "Feb 27, 2025"),
-  new LanguageData("erlang", "_", "Feb 27, 2025"),
-  new LanguageData("crystal", "_", "Feb 27, 2025"),
-  new LanguageData("reason", "_", "Feb 27, 2025"),
-  // Shell languages
-  new LanguageData("bash", "_", "Feb 27, 2025"),
-  new LanguageData("powershell", "_", "Feb 27, 2025"),
-  new LanguageData("zsh", "_", "Feb 27, 2025"),
-  new LanguageData("fish", "Fish Shell", "Feb 27, 2025"),
-  // Markup languages
-  new LanguageData("html", "_", "Feb 27, 2025"),
-  new LanguageData("xml", "_", "Feb 27, 2025"),
-  new LanguageData("yaml", "_", "Feb 27, 2025"),
-  new LanguageData("json", "_", "Feb 27, 2025"),
-  new LanguageData("markdown", "_", "Feb 27, 2025"),
-  // Style languages
-  new LanguageData("css", "_", "Feb 27, 2025"),
-  new LanguageData("sass", "_", "Feb 27, 2025"),
-];
+const all = getAllCheatsheets();
+
+const languagesData: CheatsheetType[] = all
+  .filter(c => c.category === "Language")
+  .map(c => new LanguageData(c.id, c.title, "Unknown", undefined, c.lastModified));
 
 export default languagesData;
