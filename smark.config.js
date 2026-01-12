cc1e58d9701a9706470b03a214fac3bdae6a0747
Adam-Elmi <Adamcade123@gmail.com>
2026-01-13 02:38:58 +0300

    created smark.config.js

---
+++ smark.config.js
@@ -0,0 +1,11 @@
+import somcheatMapper from "./mapper/somcheatMapper.js";
+
+const config = {
+    outputDir: "./",
+    outputFile: "adam",
+    mode: "custom",
+    format: "mdx",
+    mappingFile: somcheatMapper
+};
+
+export default config;
