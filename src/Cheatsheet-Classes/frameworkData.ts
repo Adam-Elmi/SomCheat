a7a77a79c3d519d06405765ba33c214080883a80
Adam-Elmi <Adamcade123@gmail.com>
2026-01-13 02:38:57 +0300

    updated frameworkData.ts

--- src/Cheatsheet-Classes/frameworkData.ts
+++ src/Cheatsheet-Classes/frameworkData.ts
@@ -1,36 +1,16 @@
 import { FrameworkData } from "./cheatsheetData";
 import type CheatsheetType from "../types/cheatsheet";
+import { getAllCheatsheets } from "../data/cheatsheets";
+
 /* 
   ------------------
   Frameworks
   ------------------
 */
-const frameworkData: CheatsheetType[] = [
-  new FrameworkData("expressjs", "Express.js", "Feb 27, 2025"),
-  new FrameworkData("electron", "_", "Feb 27, 2025", "desktop"),
-  new FrameworkData("reactnative", "React Native", "Feb 27, 2025", "mobile"),
-  new FrameworkData("flutter", "_", "Feb 27, 2025", "mobile"),
-  new FrameworkData("vue", "Vue.js", "Feb 27, 2025", "web"),
-  new FrameworkData("angular", "_", "Feb 27, 2025", "web"),
-  new FrameworkData("svelte", "_", "Feb 27, 2025", "web"),
-  new FrameworkData("ember", "Ember.js", "Feb 27, 2025", "web"),
-  new FrameworkData("nextjs", "Next.js", "Feb 27, 2025", "web"),
-  new FrameworkData("nuxtjs", "Nuxt.js", "Feb 27, 2025", "web"),
-  new FrameworkData("spring", "_", "Feb 27, 2025"),
-  new FrameworkData("django", "_", "Feb 27, 2025"),
-  new FrameworkData("laravel", "_", "Feb 27, 2025"),
-  new FrameworkData("rails", "Ruby on Rails", "Feb 27, 2025"),
-  new FrameworkData("flask", "_", "Feb 27, 2025"),
-  new FrameworkData("gin", "_", "Feb 27, 2025"),
-  new FrameworkData("phoenix", "_", "Feb 27, 2025"),
-  new FrameworkData("dotnet", ".NET", "Feb 27, 2025"),
-  new FrameworkData("jest", "_", "Feb 27, 2025"),
-  new FrameworkData("mocha", "_", "Feb 27, 2025"),
-  new FrameworkData("pytest", "_", "Feb 27, 2025"),
-  new FrameworkData("pytorch", "_", "Feb 27, 2025"),
-  new FrameworkData("tailwind", "Tailwind CSS", "Feb 27, 2025"),
-  new FrameworkData("bootstrap", "_", "Feb 27, 2025"),
-  new FrameworkData("bulma", "_", "Feb 27, 2025"),
-];
+const all = getAllCheatsheets();
+
+const frameworkData: CheatsheetType[] = all
+  .filter(c => c.category === "Framework")
+  .map(c => new FrameworkData(c.id, c.title, "Unknown", undefined, c.lastModified));
 
 export default frameworkData;
