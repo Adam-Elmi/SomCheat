2e4a0efa749eee0b197daa0f4b945dbce6c75d90
Adam-Elmi <Adamcade123@gmail.com>
2026-02-04 03:32:29 +0300

    Update Other_Icons.tsx

--- src/ReactComponent/icons/Other_Icons.tsx
+++ src/ReactComponent/icons/Other_Icons.tsx
@@ -94,20 +94,24 @@ export function DateIcon({
   color = defaultProps.color,
 }) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={dimension}
-      height={dimension}
-      viewBox="0 0 16 16"
-    >
-      <path
-        fill={color}
-        d="M14.5 16h-13C.67 16 0 15.33 0 14.5v-12C0 1.67.67 1 1.5 1h13c.83 0 1.5.67 1.5 1.5v12c0 .83-.67 1.5-1.5 1.5M1.5 2c-.28 0-.5.22-.5.5v12c0 .28.22.5.5.5h13c.28 0 .5-.22.5-.5v-12c0-.28-.22-.5-.5-.5z"
-      ></path>
-      <path
-        fill={color}
-        d="M4.5 4c-.28 0-.5-.22-.5-.5v-3c0-.28.22-.5.5-.5s.5.22.5.5v3c0 .28-.22.5-.5.5m7 0c-.28 0-.5-.22-.5-.5v-3c0-.28.22-.5.5-.5s.5.22.5.5v3c0 .28-.22.5-.5.5m4 2H.5C.22 6 0 5.78 0 5.5S.22 5 .5 5h15c.28 0 .5.22.5.5s-.22.5-.5.5"
-      ></path>
+    <svg xmlns="http://www.w3.org/2000/svg" width={dimension} height={dimension} viewBox="0 0 36 36">
+      <path fill={color} d="M32.25 6H29v2h3v22H4V8h3V6H3.75A1.78 1.78 0 0 0 2 7.81v22.38A1.78 1.78 0 0 0 3.75 32h28.5A1.78 1.78 0 0 0 34 30.19V7.81A1.78 1.78 0 0 0 32.25 6" className="clr-i-outline clr-i-outline-path-1"></path>
+      <path fill={color} d="M8 14h2v2H8z" className="clr-i-outline clr-i-outline-path-2"></path>
+      <path fill={color} d="M14 14h2v2h-2z" className="clr-i-outline clr-i-outline-path-3"></path>
+      <path fill={color} d="M20 14h2v2h-2z" className="clr-i-outline clr-i-outline-path-4"></path>
+      <path fill={color} d="M26 14h2v2h-2z" className="clr-i-outline clr-i-outline-path-5"></path>
+      <path fill={color} d="M8 19h2v2H8z" className="clr-i-outline clr-i-outline-path-6"></path>
+      <path fill={color} d="M14 19h2v2h-2z" className="clr-i-outline clr-i-outline-path-7"></path>
+      <path fill={color} d="M20 19h2v2h-2z" className="clr-i-outline clr-i-outline-path-8"></path>
+      <path fill={color} d="M26 19h2v2h-2z" className="clr-i-outline clr-i-outline-path-9"></path>
+      <path fill={color} d="M8 24h2v2H8z" className="clr-i-outline clr-i-outline-path-10"></path>
+      <path fill={color} d="M14 24h2v2h-2z" className="clr-i-outline clr-i-outline-path-11"></path>
+      <path fill={color} d="M20 24h2v2h-2z" className="clr-i-outline clr-i-outline-path-12"></path>
+      <path fill={color} d="M26 24h2v2h-2z" className="clr-i-outline clr-i-outline-path-13"></path>
+      <path fill={color} d="M10 10a1 1 0 0 0 1-1V3a1 1 0 0 0-2 0v6a1 1 0 0 0 1 1" className="clr-i-outline clr-i-outline-path-14"></path>
+      <path fill={color} d="M26 10a1 1 0 0 0 1-1V3a1 1 0 0 0-2 0v6a1 1 0 0 0 1 1" className="clr-i-outline clr-i-outline-path-15"></path>
+      <path fill={color} d="M13 6h10v2H13z" className="clr-i-outline clr-i-outline-path-16"></path>
+      <path fill="none" d="M0 0h36v36H0z"></path>
     </svg>
   );
 }
