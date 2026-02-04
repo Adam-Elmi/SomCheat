1718a19b01c0004632bd5cb4919a42a3f7021ff6
Adam-Elmi <Adamcade123@gmail.com>
2026-02-04 03:31:53 +0300

    Update databaseData.ts

--- src/Cheatsheet-Classes/databaseData.ts
+++ src/Cheatsheet-Classes/databaseData.ts
@@ -11,6 +11,6 @@ const all = getAllCheatsheets();
 
 const databasesData: CheatsheetType[] = all
   .filter(c => c.category === "Database")
-  .map(c => new DatabaseData(c.id, c.title, "Unknown", undefined, c.lastModified));
+  .map(c => new DatabaseData(c.id, c.title, c.lastModified || "Unknown", undefined));
 
 export default databasesData;
