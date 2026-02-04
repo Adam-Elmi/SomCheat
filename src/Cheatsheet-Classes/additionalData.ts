4bdf82f627c0261c626e0663dc95b76ac9e8324f
Adam-Elmi <Adamcade123@gmail.com>
2026-02-04 03:31:45 +0300

    Update additionalData.ts

--- src/Cheatsheet-Classes/additionalData.ts
+++ src/Cheatsheet-Classes/additionalData.ts
@@ -11,6 +11,6 @@ const all = getAllCheatsheets();
 
 const additionalData: CheatsheetType[] = all
   .filter(c => c.category === "Additional" || c.category === "Other")
-  .map(c => new AdditionalData(c.id, c.title, "Unknown", undefined, c.lastModified));
+  .map(c => new AdditionalData(c.id, c.title, c.lastModified || "Unknown", undefined));
 
 export default additionalData;
