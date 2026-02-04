37ffa7dad3553acca7c77e546489d44d0b07403a
Adam-Elmi <Adamcade123@gmail.com>
2026-02-04 03:31:50 +0300

    Update conceptData.ts

--- src/Cheatsheet-Classes/conceptData.ts
+++ src/Cheatsheet-Classes/conceptData.ts
@@ -11,6 +11,6 @@ const all = getAllCheatsheets();
 
 const conceptData: CheatsheetType[] = all
   .filter(c => c.category === "Concept")
-  .map(c => new ConceptData(c.id, c.title, "Unknown", undefined, c.lastModified));
+  .map(c => new ConceptData(c.id, c.title, c.lastModified || "Unknown", undefined));
 
 export default conceptData;
