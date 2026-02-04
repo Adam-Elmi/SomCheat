45634cc4509fab4c31c54dfdd80251ef8bd77770
Adam-Elmi <Adamcade123@gmail.com>
2026-02-04 03:32:02 +0300

    Update languageData.ts

--- src/Cheatsheet-Classes/languageData.ts
+++ src/Cheatsheet-Classes/languageData.ts
@@ -11,6 +11,6 @@ const all = getAllCheatsheets();
 
 const languagesData: CheatsheetType[] = all
   .filter(c => c.category === "Language")
-  .map(c => new LanguageData(c.id, c.title, "Unknown", undefined, c.lastModified));
+  .map(c => new LanguageData(c.id, c.title, c.lastModified || "Unknown", undefined));
 
 export default languagesData;
