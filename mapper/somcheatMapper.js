2fff008ae868e8837978b6679c3b7bbe2c9f9227
Adam-Elmi <Adamcade123@gmail.com>
2026-03-23 21:01:09 +0300

    Improved the mapper

--- mapper/somcheatMapper.js
+++ mapper/somcheatMapper.js
@@ -1,183 +1,138 @@
 import { Mapper, MDX } from "sommark";
 
-const somcheatMapper = new Mapper();
-somcheatMapper.outputs = MDX.outputs;
-somcheatMapper.removeOutput("table");
-
-const somcheat_imports = somcheatMapper.raw_js_imports([
-	{ name: "Intro", path: "../../Somcheat-Components/Intro.astro" },
-	{ name: "Structure", path: "../../Somcheat-Components/Structure.astro" },
-	{ name: "Section", path: "../../Somcheat-Components/Section.astro" },
-	{ name: "Code", path: "../../Somcheat-Components/Code.astro" },
-	{ name: "Message", path: "../../Somcheat-Components/Message.astro" },
-	{ name: "References", path: "../../Somcheat-Components/References.astro" },
-	{ name: "Table", path: "../../Somcheat-Components/Table.astro" },
-	{ name: "Steps", path: "../../Somcheat-Components/Steps.astro" },
-	{ name: "Step", path: "../../Somcheat-Components/Step.astro" },
-	{ name: "Colorize", path: "../../Somcheat-Components/Colorize.astro" },
-	{ name: "ImageComponent", path: "../../Somcheat-Components/ImageComponent.astro" }
-]);
-
-const { tag, md, safeArg, makeFrontmatter } = somcheatMapper;
-
+const somcheat_mapper = new Mapper();
+somcheat_mapper.inherit(MDX);
+const { tag, safeArg, md } = somcheat_mapper;
+// Headings
+["h1", "h2", "h3", "h4", "h5", "h6"].forEach(heading => {
+	somcheat_mapper.register(
+		heading,
+		({ content }) => {
+			const lvl = heading[1] && typeof Number(heading[1]) === "number" ? heading[1] : 1;
+			return md.heading(content.trim(), lvl);
+		},
+		{ type: "Block" }
+	);
+});
 // ========================================================================== //
-//  Imports and FrontMatter                                                   //
+//  Components                                                                //
 // ========================================================================== //
-somcheatMapper.register(
-	["Header", "header"],
-	({ args }) => {
-		return (
-			makeFrontmatter({
-				layout: "../../Layouts/SecondLayout.astro",
-				title: safeArg(args, undefined, "title", null, null, ""),
-				version: safeArg(args,undefined, "version", null, null, ""),
-				release: safeArg(args,undefined, "release", null, null, ""),
-				author: safeArg(args,undefined, "author", null, null, ""),
-				category: safeArg(args,undefined, "category", null, null, "")
-			}) + somcheat_imports
-		);
-	},
-	{
-		rules: {
-			args: {
-				required: ["title", "version", "release", "author", "category"]
-			}
+// Intro Component
+// Structure Component
+// Section Component
+// Table Component
+// Cell Component
+// Row Component
+// Message Component
+// References Component
+// Reference Component
+// Steps Component
+// Step Component
+const ids = ["Intro", "Structure", "Section", "Table", "Row", "Cell", "Message", "References", "Reference", "Steps", "Step"];
+for (const id of ids) {
+	somcheat_mapper.register(id, ({ args, content }) => {
+		const element = tag(id);
+		switch (id) {
+			case "Message":
+				element.props([{ __type__: "string", type: safeArg(args, 0, "type", null, null, "note") }]);
+				break;
+			case "Reference":
+				element.props([
+					{
+						__type__: "string",
+						title: safeArg(args, null, "title", null, null, "-")
+					},
+					{
+						__type__: "string",
+						url: safeArg(args, null, "url", null, null, "#")
+					}
+				]);
+				break;
+			case "Steps":
+				element.props([
+					{
+						__type__: "string",
+						title: safeArg(args, null, "title", null, null, "")
+					}
+				]);
+				break;
+			case "Step":
+				element.props([
+					{
+						__type__: "string",
+						title: safeArg(args, null, "title", null, null, "")
+					}
+				]);
+				break;
 		}
-	}
-);
-// ========================================================================== //
-//  Into Component                                                            //
-// ========================================================================== //
-somcheatMapper.register(["Intro", "intro"], ({ content }) => {
-  content = content;
-	return tag("Intro").body(content);
-});
-// ========================================================================== //
-//  Structure Component                                                       //
-// ========================================================================== //
-somcheatMapper.register(["Structure", "structure"], ({ content }) => {
-	return tag("Structure").body(content);
-});
-// ========================================================================== //
-//  Section Component                                                         //
-// ========================================================================== //
-somcheatMapper.register(["Section", "section"], ({ content }) => {
-	return tag("Section").body(content);
-});
-// ========================================================================== //
-//  Code Component                                                            //
-// ========================================================================== //
-somcheatMapper.register(
-	["Code", "code"],
+		return element.body(content);
+	}, {type: "Block"});
+}
+// Table Header
+somcheat_mapper.register("Headers", ({ content }) => {
+	return tag("TableHeader").body(content);
+}, {type: "Block"});
+// Header
+somcheat_mapper.register("Header", ({ content }) => {
+	return tag("Header").body(content);
+}, {type: "Block"});
+// Table body
+somcheat_mapper.register("Body", ({ content }) => {
+	return tag("TableBody").body(content);
+}, {type: "Block"});
+// Code Component
+somcheat_mapper.register(
+	"Code",
 	({ args, content }) => {
-    const lang = safeArg(args, 0, "lang", null, null, "text");
-		const filename = safeArg(args, 1, "filename", null, null, "");
-		const code = md.codeBlock(content, lang);
+		const lang = safeArg(args, 0, "lang", null, null, "text");
 		return tag("Code")
-			.attributes(filename ? { filename } : null)
-			.body(code);
-	},
-	{ escape: false }
-);
-// ========================================================================== //
-//  Message Component                                                         //
-// ========================================================================== //
-somcheatMapper.register(["Message", "message"], ({ args, content }) => {
-	return tag("Message").attributes({ type: safeArg(args, 0, "type", null, null, "note") }).body(content);
-});
-// ========================================================================== //
-//  Table Component                                                           //
-// ========================================================================== //
-somcheatMapper.register(["Table", "table"], ({ args, content }) => {
-	content = content.split("\n").filter(line => line !== "");
-	return tag("Table")
-		.props([
-			{
-				__type__: "other",
-				headers: JSON.stringify(args)
-			},
-			{
-				__type__: "other",
-				body: JSON.stringify(
-					content.map(line => {
-						let rowData = line.split(",");
-						rowData = rowData.map(r => {
-							r = r.trim();
-							return r;
-						});
-						return { row: rowData };
-					})
-				)
-			}
-		])
-		.selfClose();
-});
-// ========================================================================== //
-//   Steps Component                                                          //
-// ========================================================================== //
-somcheatMapper.register(["Steps", "steps"], ({ args, content }) => {
-	return tag("Steps")
-		.attributes({
-			title: safeArg(args, 0, "title", null, null, ""),
-			description: safeArg(args, 1, "description", null, null, "")
-		})
-		.body(content);
-});
-// ========================================================================== //
-//  Step Component                                                            //
-// ========================================================================== //
-somcheatMapper.register(["Step", "step"], ({ args, content }) => {
-	return tag("Step").attributes({ title: safeArg(args, 0, "title", null, null, "") }).body(content.trim());
-});
-// ========================================================================== //
-//  References Component                                                      //
-// ========================================================================== //
-somcheatMapper.register(
-	["References", "references"],
-	({ content }) => {
-		content = content.split("\n").filter(line => line !== "");
-		return tag("References")
 			.props([
 				{
-					__type__: "other",
-					references: JSON.stringify(
-						content.map(ref => {
-							ref = ref.split(",").filter(value => value != "");
-							return { title: ref[0], url: ref[1], description: ref[2] };
-						}),
-						null,
-						2
-					)
+					__type__: "string",
+					filename: safeArg(args, 1, "filename", null, null, "")
 				}
 			])
-			.selfClose();
+			.body("\n```" + lang + "" + content.trim() + "```\n");
 	},
-	{ escape: false }
+	{ escape: false, type: ["Block", "AtBlock"] }
 );
-// ========================================================================== //
-//  Colorize Component                                                        //
-// ========================================================================== //
-somcheatMapper.register(["Colorize", "colorize"], ({ args, content }) => {
-	return tag("Colorize")
+//  Image Component
+somcheat_mapper.register("Image", ({ args, content }) => {
+	return tag("ImageComponent")
 		.props([
 			{
 				__type__: "string",
-				color: safeArg(args, undefined, "color", null, null, "inherit")
+				src: safeArg(args, null, "src", null, null, "")
 			},
 			{
-				__type__: "other",
-				is_bold: safeArg(args, undefined, "is_bold", null, null, false)
+				__type__: "string",
+				alt: safeArg(args, null, "alt", null, null, "")
 			},
 			{
-				__type__: "other",
-				is_italic: safeArg(args, undefined, "is_italic", null, null, false)
+				__type__: "string",
+				parentClass: safeArg(args, null, "parentClass", null, null, "")
 			},
 			{
-				__type__: "other",
-				is_both: safeArg(args, undefined, "is_both", null, null, false)
+				__type__: "string",
+				caption: safeArg(args, null, "caption", null, null, "")
 			}
 		])
-		.body(content.trim());
-});
+		.body(content);
+}, {type: "Block"});
+// Inline Style
+somcheat_mapper.register("style", ({ args, content }) => {
+	let style = safeArg(args, 0, null, null, null, "");
+	if (style.includes("somcheat_blue")) {
+		style = style.replace("somcheat_blue", "#2a7dfd");
+	}
+	return tag("span")
+		.props([
+			{
+				__type__: "string",
+				style
+			}
+		])
+		.body(content);
+}, {type: "Inline"});
 
-export default somcheatMapper;
+export default somcheat_mapper;
